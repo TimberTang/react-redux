7a56d03d2eb2d434829a32b17487815880053b63
TimberTang <ming.tang@atomefin.com.cn>
2020-12-30 16:07:46 +0800

    fix:wq

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
+import {changeInputAction, deleteItemAction, addItemAction} from './store/actionCreaters.js'
 
 
 class TodoList extends Component {
@@ -39,26 +40,29 @@ class TodoList extends Component {
      );
   }
   changeInputValue(e) {
-      const action = {
-          type: 'changeInput',
-          value: e.target.value
-      }
+    //   const action = {
+    //     type: CHANGE_INPUT,
+    //       value: e.target.value
+    //   }
+        const action = changeInputAction(e.target.value)
       store.dispatch(action)
   }
   storeChagee () {
       this.setState(store.getState())
   }
   addTodo() {
-    const action = {
-        type: 'addItem', 
-    }
+    // const action = {
+    //     type: ADD_ITEM, 
+    // }
+    const action = addItemAction()
     store.dispatch(action)
   }
   deleItem(index) {
-      const action = {
-          type: 'deleteItem', 
-          index: index
-      }
+    //   const action = {
+    //       type: DELETE_ITEM, 
+    //       index: index
+    //   }
+        const action = deleteItemAction(index)
       store.dispatch(action)
   }
 }
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,27 +1,30 @@
 
+import {CHANGE_INPUT, DELETE_ITEM, ADD_ITEM} from './actionTypes'
+
+
 const defaultState = {
-inputValue: 'Write something', 
-  list: [
-    '八点开会',
-    '九点沟通',
-    '写代码'
-]
+	inputValue: 'Write something', 
+	list: [
+		'八点开会',
+		'九点沟通',
+		'写代码'
+	]
 }
 
 export default function(state = defaultState, action) {
   // reducer 只能接受state， 不能改变state
-  if (action.type ==='changeInput') {
+  if (action.type === CHANGE_INPUT) {
 	  let newState = JSON.parse(JSON.stringify(state)) // 拷贝
 	  newState.inputValue = action.value
 	  return newState
   }
-  if (action.type === 'addItem') {
+  if (action.type === ADD_ITEM) {
 	let newState = JSON.parse(JSON.stringify(state)) // 拷贝
 	newState.list.push(newState.inputValue)
 	newState.inputValue = ''
 	return newState
   }
-  if (action.type === 'deleteItem') {
+  if (action.type === DELETE_ITEM) {
 	let newState = JSON.parse(JSON.stringify(state)) // 拷贝
 	newState.list.splice(action.index, 1)
 	return newState
