7a2428eddbd422f460687f253deabbe6d9cecd9c
TimberTang <ming.tang@atomefin.com.cn>
2021-01-06 15:18:04 +0800

    react redux

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
-import store from './store/index'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
-        this.state = store.getState();
     }
     render() {
         return (
             <div>
-                <input value ={this.state.inputValue}></input>
-                <button>提交</button>
+                <input value ={this.props.inputValue} onChange={this.props.inputChange}></input>
+                <button onClick={this.props.addTodo}>提交</button>
                 <ul>
-                    <li>timber</li>
+                    <li>{this.props.list.length}</li>
                 </ul>
             </div>
         );
     }
+    
 }
 
-export default TodoList;
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+const despathToProps = (dispatch) => {
+    return {
+        inputChange(e) {
+            let action = {
+                type: 'input_change', 
+                value: e.target.value
+            }
+            dispatch(action)
+        },
+        addTodo() {
+            let action = {
+                type: 'todo_add', 
+            }
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(stateToProps, despathToProps)(TodoList);
--- demo02/src/store/reducer.js
+++ demo02/src/store/reducer.js
@@ -1,4 +1,4 @@
-
+ 
 const defaultState = {
   inputValue: "Hello TimberTang",
   list: [],
@@ -6,7 +6,23 @@ const defaultState = {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState, _action) => {
+  if(_action.type === 'input_change')
+  {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = _action.value
+    return newState
+  }
+  if(_action.type === 'todo_add')
+  {
+    console.log(state.inputValue);
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(state.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
   return state
 }
 
 // react-redux 插件
+
+
